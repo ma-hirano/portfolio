6fb7aeb387faa385612ce109e07ff9b7afaa7711
ma-hirano <m.hirano@ebacorp.jp>
2022-02-03 01:46:49 +0900

    add: other page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <Tutorial/>
+    <div>
+      <Header/>
+      <Tutorial/>
+    </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
+<script lang='ts'>
+import Vue from 'vue';
+import '@/assets/scss/resetCss.css'
+import '@/assets/scss/style.scss'
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  name: 'IndexPage',
+});
 </script>
+
+<style lang="scss">
+  
+</style>
