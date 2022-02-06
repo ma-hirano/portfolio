81a7c0083baf0935818661250b3de3b2910ae0d5
ma-hirano <m.hirano@ebacorp.jp>
2022-02-06 19:38:32 +0900

    add: Space page

--- pages/space.vue
+++ pages/space.vue
@@ -7,10 +7,8 @@
 
 <script lang='ts'>
 import Vue from 'vue';
-import '@/assets/scss/resetCss.css'
 import '@/assets/scss/style.scss'
 export default Vue.extend({
-  name: 'Space',
 });
 </script>
 
